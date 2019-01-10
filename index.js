6f9e46951679a41b991097b7f9d7cb5a6fdd4ed6
Monika Gharat <monika.2006.it@gmail.com>
2019-01-11 00:38:12 +0530

    add google maps and slider

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ $(".cross").click(function() {
   $(".menu").hide();
   $("body").css("overflow", "");
 });
+
+function initMap() {
+  // The location of Uluru
+  var uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map($(".mapimage")[0], { zoom: 6, center: uluru });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+}
+
+if ($("#slider-placeholder").slippry) {
+  $("#slider-placeholder").slippry();
+}
